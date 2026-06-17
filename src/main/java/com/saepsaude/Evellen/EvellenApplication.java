33c72a9a508c29254bd36725dbae08f8c4bdb0dd
EvellenDk <dominikevellen0801@gmail.com>
2026-06-17 17:21:56 -0300

    Add files via upload

---
+++ src/main/java/com/saepsaude/Evellen/EvellenApplication.java
@@ -0,0 +1,13 @@
+package com.saepsaude.Evellen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvellenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvellenApplication.class, args);
+	}
+
+}
